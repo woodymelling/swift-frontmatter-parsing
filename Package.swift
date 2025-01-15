6e5611d7acd41d3ffd8019778e8641cae671aaf5
Woodrow Melling <woodymelling@gmail.com>
2025-01-14 16:27:47 -0800

    🌱

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FrontmatterParsing",
+    platforms: [
+      .iOS(.v13),
+      .macOS(.v12),
+      .tvOS(.v13),
+      .watchOS(.v6),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FrontmatterParsing",
+            targets: ["FrontmatterParsing"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/woodymelling/swift-parsing", from: "0.1.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FrontmatterParsing",
+            dependencies: [
+                .product(name: "Parsing", package: "swift-parsing"),
+                .product(name: "Yams", package: "yams")
+            ]
+        ),
+    ]
+)
---
+++ Sources/FrontmatterParsing/FrontmatterParsing.swift
@@ -0,0 +1,103 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+//
+//  MarkdownWithFrontMatterConversion.swift
+//  OpenFestival
+//
+//  Created by Woodrow Melling on 10/25/24.
+//
+
+import Parsing
+import Conversions
+import Foundation
+
+
+public struct MarkdownWithFrontMatter<FrontMatter> {
+    public var frontMatter: FrontMatter?
+    public var body: String?
+}
+
+extension MarkdownWithFrontMatter: Equatable where FrontMatter: Equatable {}
+
+
+
+extension MarkdownWithFrontMatter {
+    struct Parser: Parsing.Parser, ParserPrinter where FrontMatter: Codable {
+        typealias Input = Substring
+        typealias Output = MarkdownWithFrontMatter
+
+        var body: some ParserPrinter<Input, Output> {
+            ParsePrint(.memberwise(MarkdownWithFrontMatter.init(frontMatter:body:))) {
+                Optionally {
+                    Parsers.FrontMatter<FrontMatter>()
+                    Whitespace(1, .vertical)
+                }
+
+                Optionally {
+                    Rest().map(.string)
+                }
+
+            }
+        }
+    }
+}
+
+extension Parsers {
+    struct FrontMatter<FrontMatter: Codable>: ParserPrinter {
+        typealias Input = Substring
+
+        // We need to be careful here, because we want to parse everything between the delimiters as Yaml.
+        // This is subtly different than parsing delimiter, yaml, delimiter.
+        var body: some ParserPrinter<Input, FrontMatter> {
+            "---"
+            Whitespace(1, .vertical)
+            PrefixUpTo("---").map(Conversions.SubstringToYaml<FrontMatter>())
+            "---"
+        }
+    }
+}
+
+import Yams
+
+struct YamlConversion<T: Codable>: Conversion {
+    typealias Input = Data
+    typealias Output = T
+
+    init(_ type: T.Type = T.self) { }
+
+    func apply(_ input: Data) throws -> T {
+        try YAMLDecoder().decode(T.self, from: input)
+    }
+
+    func unapply(_ output: T) throws -> Data {
+        try Data(YAMLEncoder().encode(output).utf8)
+    }
+}
+
+extension Conversions {
+    struct SubstringToYaml<T: Codable>: Conversion {
+        var body: some Conversion<Substring, T> {
+            Conversions.SubstringToString()
+            Conversions.DataToString().inverted()
+            YamlConversion<T>()
+        }
+    }
+}
+
+public struct MarkdownWithFrontMatterConversion<T: Codable>: Conversion {
+    public typealias Input = String
+    public typealias Output = MarkdownWithFrontMatter<T>
+
+    public func apply(_ input: String) throws -> MarkdownWithFrontMatter<T> {
+        return try MarkdownWithFrontMatter.Parser().parse(input)
+    }
+
+    public func unapply(_ output: MarkdownWithFrontMatter<T>) throws -> String {
+        var outputString: Substring = ""
+
+        try MarkdownWithFrontMatter.Parser().print(output, into: &outputString)
+
+        return String(outputString)
+    }
+}
